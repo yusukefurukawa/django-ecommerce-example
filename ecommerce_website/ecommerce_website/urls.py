8e365f0f2658160d6b0b907989d0d5264ebd99f7
h-ashino <h-ashino@sakura.ad.jp>
2016-11-02 22:08:14 +0900

    商品一覧URLルーティングの追加

--- ecommerce_website/ecommerce_website/urls.py
+++ ecommerce_website/ecommerce_website/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from ecommerce import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^ec/list/', views.index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
