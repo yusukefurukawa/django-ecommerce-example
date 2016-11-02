5c676716fe0d0a2d83430afeaea145589454ae71
h-ashino <h-ashino@sakura.ad.jp>
2016-11-02 19:39:21 +0900

    すべてのホストからのアクセスを許可する設定を追加、アプリケーションの追加。

--- ecommerce_website/ecommerce_website/settings.py
+++ ecommerce_website/ecommerce_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_07(^fox5yfj(g+!&*3+1d)fm4gjj8bx)yol8r#4=m_s=^fjt%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce'
 ]
 
 MIDDLEWARE = [
